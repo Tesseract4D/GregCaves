d47bc7fb4d144636a9c6a03436465af90176ea8e
梁弘毅 <55395193+tesseract4d@users.noreply.github.com>
2024-07-16 14:53:05 +0800

    Mod compat & config

--- src/main/java/mods/tesseract/gregcaves/fix/FixesGC.java
+++ src/main/java/mods/tesseract/gregcaves/fix/FixesGC.java
@@ -1,53 +1,57 @@
 package mods.tesseract.gregcaves.fix;
 
-import mods.tesseract.gregcaves.world.ChunkGeneratorGC;
+import mods.tesseract.gregcaves.GregCaves;
 import net.minecraft.block.Block;
 import net.minecraft.init.Blocks;
 import net.minecraft.world.World;
-import net.minecraft.world.WorldType;
 import net.minecraft.world.biome.BiomeDecorator;
-import net.minecraft.world.chunk.IChunkProvider;
-import net.minecraft.world.gen.ChunkProviderFlat;
-import net.minecraft.world.gen.MapGenCaves;
+import net.minecraft.world.biome.BiomeGenBase;
 import net.minecraft.world.gen.MapGenRavine;
+import net.minecraft.world.gen.feature.WorldGenMinable;
 import net.tclproject.mysteriumlib.asm.annotations.EnumReturnSetting;
 import net.tclproject.mysteriumlib.asm.annotations.Fix;
 import net.tclproject.mysteriumlib.asm.annotations.LocalVariable;
 
-import static net.minecraft.world.WorldType.FLAT;
+import java.util.Random;
 
 public class FixesGC {
-    @Fix(returnSetting = EnumReturnSetting.ALWAYS)
-    public static IChunkProvider getChunkGenerator(WorldType c, World world, String generatorOptions) {
-        return c == FLAT ? new ChunkProviderFlat(world, world.getSeed(), world.getWorldInfo().isMapFeaturesEnabled(), generatorOptions) : new ChunkGeneratorGC(world, world.getSeed(), world.getWorldInfo().isMapFeaturesEnabled());
-    }
-
     @Fix(insertOnExit = true, targetMethod = "<init>")
     public static void BiomeDecorator(BiomeDecorator c) {
-        c.generateLakes = false;
+        if (GregCaves.disableUndergroundLiquid)
+            c.generateLakes = false;
     }
 
-    @Fix(returnSetting = EnumReturnSetting.ALWAYS, insertOnLine = 9)
-    public static void digBlock(MapGenCaves c, Block[] data, int index, int x, int y, int z, int chunkX, int chunkZ, boolean foundTop, @LocalVariable(index = 10) Block top, @LocalVariable(index = 11) Block filler, @LocalVariable(index = 12) Block block) {
-        if (block == Blocks.stone || block == filler || block == top) {
-            if (y < 2) {
-                data[index] = Blocks.lava;
-            } else {
-                data[index] = null;
-
-                if (foundTop && data[index - 1] == filler) {
-                    data[index - 1] = top;
-                }
+    @Fix(insertOnExit = true)
+    public static void genBiomeTerrain(BiomeGenBase c, World w, Random r, Block[] blocks, byte[] b, int cx, int cz, double d) {
+        if (GregCaves.smoothBedrock) {
+            int i1 = cx & 15;
+            int j1 = cz & 15;
+            int k1 = blocks.length / 256;
+            for (int l1 = 5; l1 > 0; --l1) {
+                int i2 = (j1 * 16 + i1) * k1 + l1;
+                if (blocks[i2] == Blocks.bedrock)
+                    blocks[i2] = Blocks.stone;
             }
         }
+    }
+
+    @Fix(returnSetting = EnumReturnSetting.ON_TRUE)
+    public static boolean generate(WorldGenMinable c, World w, Random r, int x, int y, int z) {
+        if (GregCaves.reduceOreGen && y < GregCaves.reduceOreGenY && w.provider.dimensionId == 0)
+            return r.nextFloat() < GregCaves.reduceOreGenRate;
+        return false;
     }
 
     @Fix(returnSetting = EnumReturnSetting.ALWAYS, insertOnLine = 9)
     public static void digBlock(MapGenRavine c, Block[] data, int index, int x, int y, int z, int chunkX, int chunkZ, boolean foundTop, @LocalVariable(index = 10) Block top, @LocalVariable(index = 11) Block filler, @LocalVariable(index = 12) Block block) {
         if (block == Blocks.stone || block == filler || block == top) {
-            data[index] = null;
-            if (foundTop && data[index - 1] == filler) {
-                data[index - 1] = top;
+            if (y < GregCaves.caveLavaLevel) {
+                data[index] = Blocks.lava;
+            } else {
+                data[index] = null;
+                if (foundTop && data[index - 1] == filler) {
+                    data[index - 1] = top;
+                }
             }
         }
     }
